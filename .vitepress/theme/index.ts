469f98563bd8f17241921b5a71ebc50d23ca0e72
saltcute <me@mizuno.ai>
2024-06-25 04:20:04 -0400

    feat: auto redirect to coressponding language of the user

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
 import './style.css'
 
-export default DefaultTheme
+export default {
+    extends: DefaultTheme,
+    Layout
+}

0bedd1d6ca25906ae3dfed1d93d5da2b13f66de2
saltcute <me@mizuno.ai>
2024-08-10 03:50:49 -0400

    feat: add vercel analytics

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vitepress'
-import locales from './locales'
+import { defineConfig } from "vitepress"
+import locales from "./locales"
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
     title: "Kasumi.js",
     description: "A KOOK bot framework in TypeScript",
-    locales: locales(), 
+    locales: locales(),
+    head: [["script", { src: "/_vercel/insights/script.js", defer: "" }]],
     themeConfig: {
         socialLinks: [
-            { icon: 'github', link: 'https://github.com/HexaNona/kasumi' },
-            { icon: 'twitter', link: 'https://twitter.com/saltcutep' }
+            { icon: "github", link: "https://github.com/HexaNona/kasumi" },
+            { icon: "twitter", link: "https://twitter.com/saltcutep" },
         ],
 
         editLink: {
-            pattern: 'https://github.com/saltcute/kasumi-doc/edit/main/:path'
-        }
-    }
+            pattern: "https://github.com/saltcute/kasumi-doc/edit/main/:path",
+        },
+    },
 })
